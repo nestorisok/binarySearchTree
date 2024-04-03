623bf0380cf9596b804dadc573bfb02a9c72b141
Nestor Garcia <nestorisok@gmail.com>
2024-04-03 11:37:49 -0700

    Everything works

--- binarySearchTree/binarySearchTree.cpp
+++ binarySearchTree/binarySearchTree.cpp
@@ -26,36 +26,50 @@ public:
 	Node* insertNode(Node* root, int data);
 	bool findNode(Node* root, int data);
 	int Largest(Node* root);
-};
+	void printSearch(Node* root, int data);
 
+};
 
 
 int main() {
 
+	int searchNum;
+
 	BST myTree;
-	Node myNode = 4;
-	myTree.insertNode(&myNode, 2);
-	myTree.insertNode(&myNode, 7);
+	Node myNode = 7;
+	myTree.insertNode(&myNode, 20);
+	myTree.insertNode(&myNode, 8);
+	myTree.insertNode(&myNode, 9);
+	myTree.insertNode(&myNode, 6);
 	myTree.insertNode(&myNode, 1);
-	myTree.insertNode(&myNode, 3);
+	myTree.insertNode(&myNode, 19);
+	myTree.insertNode(&myNode, 15);
+	myTree.insertNode(&myNode, 2);
+	myTree.insertNode(&myNode, 10);
 	myTree.printTree(&myNode);
 	
 	cout << "\nLargest: " << myTree.Largest(&myNode) << endl;
 
-	int searchNum = 5;
 
-	if (myTree.findNode(&myNode, searchNum) == NULL) {
-		cout << searchNum << " was not found." << endl;
-	}
-	else {
-		cout << searchNum << " was found." << endl;
-	}
+	cout << "\nInsert a number to search: ";
+	cin >> searchNum;
+	myTree.printSearch(&myNode, searchNum);
 
+	cout << "\nInsert a number to search: ";
+	cin >> searchNum;
+	myTree.printSearch(&myNode, searchNum);
 
 	return 0;
 }
 
-
+void BST::printSearch(Node* root, int data) {
+	if (findNode(root, data) == NULL) {
+		cout << data << " was not found." << endl;
+	}
+	else {
+		cout << data << " was found." << endl;
+	}
+}
 
 void BST::printTree(Node* root) { // post order travel // Currently works
 	if (root != NULL) {
